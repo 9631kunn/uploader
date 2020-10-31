27b097de4495a34429e9326255410ed18d76b90b
9631kunn <infourbh+1@gmail.com>
2020-10-31 19:03:15 +0900

    add: func

--- ui.js
+++ ui.js
@@ -1,8 +1,15 @@
 (function () {
-  const a = document.querySelector(".upload__area");
-  const i = document.querySelector(".upload__file--input");
-  const b = document.querySelector(".upload__button");
+  // 使うやつ
+  const d = document,
+    a = d.querySelector(".upload__area"),
+    r = d.querySelector(".upload__result"),
+    i = d.querySelector(".upload__file--input"),
+    b = d.querySelector(".upload__button--upload"),
+    c = d.querySelector(".upload__button--copy"),
+    o = d.querySelector(".upload__option");
+  console.log(r);
 
+  // UI用
   a.addEventListener("dragover", (e) => {
     e.preventDefault();
     a.classList.add("active");
@@ -17,17 +24,49 @@
     i.files = e.dataTransfer.files;
     // デザイン修正のためここのタイミングでファイル名取得してspanとかで要素挿入
   });
-  b.addEventListener("click", () => {
-    const req = new XMLHttpRequest();
-    const data = new FormData(document.querySelector(".upload__form"));
-    const path = location.href + "upload.php";
+  // アップロード処理
+  b.addEventListener("click", (e) => {
+    e.preventDefault();
+    a.classList.add("fadeOut");
+    b.classList.add("fadeOut");
+    setTimeout(() => {
+      a.classList.add("none");
+      b.classList.add("none");
+    }, 1500);
+    const req = new XMLHttpRequest(),
+      data = new FormData(d.querySelector(".upload__form")),
+      path = location.href + "upload.php";
     req.open("POST", path);
     req.send(data);
     req.onreadystatechange = () => {
       if (req.readyState !== 4 || req.status !== 200) return;
-      console.log(req.responseText);
+      r.classList.remove("none");
+      c.classList.remove("none");
+      r.classList.add("fadeIn");
+      c.classList.add("fadeIn");
+      o.classList.add("none");
+      d.querySelector(".upload__title").textContent = "Yay! Uploaded!!";
+      r.insertAdjacentHTML("afterbegin", req.responseText);
     };
-    req.onprogress = (e) => {};
+  });
+  // クリップボードにコピー
+  c.addEventListener("click", (e) => {
+    e.preventDefault();
+    // リンク一覧
+    const links = d.querySelectorAll(".upload__result--link"),
+      linkList = [];
+    links.forEach((link) => {
+      linkList.push(link.href);
+    });
+    // コピー処理
+    const linkText = d.createElement("textarea");
+    linkText.textContent = linkList.join("\n");
+    d.body.appendChild(linkText);
+    linkText.select();
+    const copy = d.execCommand("copy");
+    d.body.removeChild(linkText);
+    // ツールチップ
+    copy ? d.querySelector(".upload__copy-success").classList.add("success") : console.log("f");
   });
 })();
 
--- upload.php
+++ upload.php
@@ -1,7 +1,5 @@
 <?php
 
-// セキュリティ的なところは.htaccessとHTMLとJS負担
-
 // 複数ファイルがアップロードされた場合、Arrayで渡ってくる
 // Arrayのlength分アップロード処理を回す（1つずつしかアップできないので）
 for($i = 0; $i < count($_FILES["upload"]["name"]); $i++)
@@ -12,14 +10,30 @@ for($i = 0; $i < count($_FILES["upload"]["name"]); $i++)
   // ファイル名変更（重複回避のため）
   $original = $_FILES["upload"]["name"][$i];
   list($filename, $extension) = explode(".", $original);
-  $copy = date("YmdHis")."_".$filename.".".$extension;
-  $result = @move_uploaded_file($tmp_file, $upload_dir.$copy);
+  if (exif_imagetype($tmp_file)) {
+    $upload_name = date("YmdHis")."_".$filename.".".$extension;
+    $result = @move_uploaded_file($tmp_file, $upload_dir.$upload_name);
+  } else {
+    $result = false;
+  }
+
+
+  // ファイル名変更処理なし
+  // if($_GET["overwrite"] === true){
+  //   echo "test";
+  // }
+  // checkbox用意してその値による分岐
+  // checkがあれば既存のファイルとアップロードされたファイルを上書き
 
   // Success / Error メッセージ
   if($result === true)
   {
-    echo $original."をアップロードしました。<br>";
-    echo "<a href=\"./data/".$copy."\" target=\"_blank\">$original</a>";
+    echo "<a href=\"./data/".$upload_name."\" class=\"upload__result--link\" target=\"_blank\">"
+    ."<img src=\"./data/".$upload_name."\" class=\"upload__result--img\" loading=\"lazy\">"
+    ."<div class=\"upload__result--filename\">"
+    .$original
+    ."</div>"
+    ."</a>";
   }
   else
   {
