be0f502044e7e9806fe637211d363d774a709d38
9631kunn <infourbh+1@gmail.com>
2020-10-31 19:04:13 +0900

    upし忘れ

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
 
+// セキュリティ的なところは.htaccessとHTMLとJS負担
+
 // 複数ファイルがアップロードされた場合、Arrayで渡ってくる
 // Arrayのlength分アップロード処理を回す（1つずつしかアップできないので）
 for($i = 0; $i < count($_FILES["upload"]["name"]); $i++)
@@ -7,21 +9,28 @@ for($i = 0; $i < count($_FILES["upload"]["name"]); $i++)
   $upload_dir = __DIR__."/data/";
   $tmp_file = $_FILES["upload"]["tmp_name"][$i];
 
-  // ファイル名変更（重複回避のため）
+  // アップされた画像
   $original = $_FILES["upload"]["name"][$i];
   list($filename, $extension) = explode(".", $original);
-  if (exif_imagetype($tmp_file)) {
-    $upload_name = date("YmdHis")."_".$filename.".".$extension;
-    $result = @move_uploaded_file($tmp_file, $upload_dir.$upload_name);
+
+  // ファイル名変更しない場合とする場合
+  if ($_POST["overwrite"]){
+    // dataディレクトリ内から同名ファイルの検索
+    $matches = preg_grep("/".$filename."/", glob("data/*"));
+    if(empty($matches)){
+      $upload_name = date("YmdHis")."_".$filename.".".$extension;
+    }else{
+      // マッチしたものがあれば一応その中で最新のものを取得する
+      $path = end($matches);
+      $format_path = str_replace('data/', '', $path);
+      $upload_name = $format_path;
+    }
   } else {
-    $result = false;
+    $upload_name = date("YmdHis")."_".$filename.".".$extension;
   }
 
-
-  // ファイル名変更処理なし
-  // if($_GET["overwrite"] === true){
-  //   echo "test";
-  // }
+  // アップロード処理
+  $result = @move_uploaded_file($tmp_file, $upload_dir.$upload_name);
   // checkbox用意してその値による分岐
   // checkがあれば既存のファイルとアップロードされたファイルを上書き
 
